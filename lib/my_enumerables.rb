7107c6b8cf9e234a6a9b0caedb4ba93b57ea327a
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-05 10:28:12 +0400

    Implement my_each_with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,12 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    self.my_each do |item|
+      yield(item, index)
+      index += 1
+    end
+  end
 end
 
 # You will first have to define my_each
